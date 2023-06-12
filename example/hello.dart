89f8fedd040238659d0c21375d05234e7244f620
BagasRozzaqFadli <BagaroFa337@gmail.com>
2023-06-12 10:42:52 +0700

    first commit

---
+++ example/hello.dart
@@ -0,0 +1,9 @@
+import 'package:belajar_dart_library/hello.dart';
+
+void main() {
+  print(sayHello("Bagas Rozzaq Fadli"));
+
+  var customer = Customer();
+  var product = Category();
+  var category = Category();
+}
